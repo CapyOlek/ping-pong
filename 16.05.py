4f8af7882c7cba07b3d80b4287d64d34ef96e4b5
CapyOlek <obolevych3@gmail.com>
2025-05-23 21:09:11 +0200

    Add files via upload

--- 16.05.py
+++ 16.05.py
@@ -14,9 +14,13 @@ win.fill(back)
 font.init()
 font1 = font.Font(None, 80)
 font = font.Font(None, 35)
+score_red = 0
+score_blue = 0
 
-lose_txt_red = font1.render("RED LOSES! ", 1, (250, 250, 250))
-lose_txt_blue = font1.render("BLUE LOSES! ", 1, (250, 250, 250))
+lose_txt_red = font1.render("POINT FOR BLUE! ", 1, (250, 250, 250))
+lose_txt_blue = font1.render("POINT FOR RED! ", 1, (250, 250, 250))
+final_lose_txt_red = font1.render("RED LOSES! ", 1, (250, 250, 250))
+final_lose_txt_blue = font1.render("BLUE LOSES! ", 1, (250, 250, 250))
 # music
 mixer.init()
 lose_sound = mixer.Sound('lose_sound.wav')
@@ -57,6 +61,7 @@ class Player(GameSprite):
 class Ball(GameSprite):
     def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
         super().__init__(player_image, player_x, player_y, size_x, size_y, player_speed)
+        self.original_image = self.image.copy()
         self.angle = 0
         self.spin_speed = 0
 
@@ -77,7 +82,7 @@ class Ball(GameSprite):
 
 player_blue = Player('blue.png', 5, win_height - 100, 60, 90, 10)
 player_red = Player('red.png', 840, win_height - 100, 60, 90, 10)
-ball = GameSprite('ball.png', win_width/2-25, win_height/2-25, 50, 50, 12)
+ball = Ball('ball.png', win_width/2-25, win_height/2-25, 50, 50, 12)
 
 
 def reset_ball():
@@ -104,7 +109,7 @@ while game:
         ball.rect.x += x_speed
         ball.rect.y += y_speed
         if ball.rect.y < 0 or ball.rect.y > win_height-50:
-            y_speed *= -1
+            y_speed *= -1.00001
             # столкновение с ракеткой blue
 
         if sprite.collide_rect(player_blue, ball):
@@ -118,27 +123,40 @@ while game:
 
         if sprite.collide_rect(player_red, ball):
             ball.rect.right = player_red.rect.left  # предотвращаем залипание
-
+            offset = (ball.rect.centery - player_red.rect.centery)/(player_red.rect.height/2)
+            y_speed += offset*2
             x_speed *= -1
+            ball.spin_hit(offset)
         if ball.rect.x < 0:
-            finish = True
-            lose_sound.play()
-            win.blit(lose_txt_blue, (win_width//2-100, win_height//2-50))
+            score_red += 1
+            if score_red >= 5:
+                finish = True
+                win.blit(final_lose_txt_blue, (win_width//2-200, win_height//2-50))
+            else:
+
+                lose_sound.play()
+                win.blit(lose_txt_blue, (win_width//2-200, win_height//2-50))
             pygame.display.update()
             pygame.time.delay(1505)
-            finish = False
             x_speed, y_speed = reset_ball()
         if ball.rect.x > win_width:
-            finish = True
-            lose_sound.play()
-            win.blit(lose_txt_red, (win_width//2-100, win_height//2-50))
+            score_blue += 1
+            if score_blue >= 5:
+                finish = True
+                win.blit(final_lose_txt_red, (win_width//2-200, win_height//2-50))
+            else:
+                lose_sound.play()
+                win.blit(lose_txt_red, (win_width//2-200, win_height//2-50))
             pygame.display.update()
             pygame.time.delay(1505)
-            finish = False
             x_speed, y_speed = reset_ball()
         # обновляем их в новом местоположении при каждой итерации цикла
+        ball.update_spin(x_speed, y_speed)
         player_blue.reset()
         player_red.reset()
         ball.reset()
+        score_txt = font1.render(f"BLUE:{score_blue} | RED:{score_red}", 1, (255, 255, 255))
+        score_txt.set_alpha(127)
+        win.blit(score_txt, (win_width//2-200, 30))
     display.update()
     clock.tick(FPS)
